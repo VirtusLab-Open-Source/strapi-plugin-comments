45eb8738e124b8d810fe00e5278a4e2cc3791dd7
Mateusz Ziarko <mziarko@virtuslab.com>
2020-07-23 21:03:51 +0200

    reworks

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -2,7 +2,7 @@ module.exports = async () => {
   // Check if the plugin users-permissions is installed because the comments needs it
   if (Object.keys(strapi.plugins).indexOf('users-permissions') === -1) {
     throw new Error(
-      'In order to make the comments plugin works the users-permissions one is required'
+      'In order to make the comments plugin work the users-permissions plugin is required'
     );
   }
 
