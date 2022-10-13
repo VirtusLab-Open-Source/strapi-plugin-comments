16cb586ff43876016ba8185dede69d23f879737e
CodeVoyager <tmsz.dnl.puch@gmail.com>
2022-10-13 16:23:12 +0200

    feat: custom fields on server-side

--- server/controllers/utils/parsers.ts
+++ server/controllers/utils/parsers.ts
@@ -1,12 +1,14 @@
 import { OnlyStrings } from "strapi-typed";
 import { FlatInput, ToBeFixed } from "../../../types";
+import PluginError from "../../utils/error";
+import { assertNotEmpty } from "../../utils/functions";
 
 export const flatInput = <T, TKeys = keyof T>(
   payload: FlatInput<OnlyStrings<TKeys>>
 ) => {
   const { relation, query, sort, pagination, fields } = payload;
 
-  const { populate = {}, ...restQuery } = query;
+  const { populate = {}, filterBy, filterByValue, ...restQuery } = query;
   const filters = restQuery?.filters || restQuery;
   const orOperator = (filters?.$or || []).filter(
     (_: ToBeFixed) => !Object.keys(_).includes("removed")
@@ -40,6 +42,30 @@ export const flatInput = <T, TKeys = keyof T>(
         },
       },
     };
+  }
+
+  if (filterBy === "DATE_CREATED") {
+    const date = new Date(filterByValue);
+
+    if (!filterByValue || Number.isNaN(+date)) {
+      throw new PluginError(400, 'Invalid date specified in "filterByValue"');
+    }
+
+    const start = date.setHours(0, 0, 0, 0);
+    const end = date.setHours(23, 59, 59, 999);
+
+    filters.createdAt = {
+      $between: [start, end],
+    };
+  }
+
+  if (filterBy === "APPROVAL_STATUS") {
+    assertNotEmpty(
+      filterByValue,
+      new PluginError(400, 'Empty "filterByValue" parameter')
+    );
+
+    filters.approvalStatus = filterByValue;
   }
 
   return {
