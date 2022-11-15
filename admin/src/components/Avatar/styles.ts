99ec162e8d004cec3ba63e470ffde5e6c0331b56
Michał Kmak <mkmak.code@gmail.com>
2022-11-15 16:50:21 +0100

    fix: change error 403 test
    Expexted result from changed test is now 404 status and message.

---
+++ admin/src/components/Avatar/styles.ts
@@ -0,0 +1,15 @@
+// @ts-nocheck
+import {Icon} from "@strapi/design-system/Icon";
+import styled from "styled-components";
+import { Box } from "@strapi/design-system/Box";
+
+
+export const AdminAvatarWrapper = styled(Box)`
+position: relative;
+`;
+
+export const AdminShield = styled(Box)`
+    position: absolute;
+    right: -30%;
+    top: -30%;
+`;
