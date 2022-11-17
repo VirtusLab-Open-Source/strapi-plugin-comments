466ca1712be3cc9372f4afa2a2d42e25af9e8681
Kamil Szewczyk <kamilszewczyk0@gmail.com>
2022-11-17 20:25:20 +0100

    feat: basic filters options added to Discover and Reports

--- admin/src/pages/Discover/utils/filtersSchema.ts
+++ admin/src/pages/Discover/utils/filtersSchema.ts
@@ -4,6 +4,34 @@ const filtersSchema = [
     metadatas: { label: "Message" },
     fieldSchema: { type: "string" },
   },
+  {
+    name: "createdAt",
+    metadatas: { label: "CreatedAt" },
+    fieldSchema: { type: "datetime" },
+  },
+  {
+    name: "updatedAt",
+    metadatas: { label: "UpdatedAt" },
+    fieldSchema: { type: "datetime" },
+  },
+  {
+    name: "authorName",
+    metadatas: {label: "Author"},
+    fieldSchema: {type: "string"},
+  },
+  {
+    name: "threadOf",
+    metadatas: {label: "threadOf"},
+    fieldSchema: {
+      type: "relation",
+      mainField: {
+        name: "id",
+        schema: {
+          type: "number"
+        }
+      }
+    }
+  }
 ];
 
 export default filtersSchema;
