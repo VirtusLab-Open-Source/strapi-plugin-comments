6de59adb51cf9fd99218a13d43e7f1a1eb9c749c
Vertudo <kpono.dk@gmail.com>
2023-04-16 12:18:05 +0400

    added RU translation

--- admin/src/translations/index.ts
+++ admin/src/translations/index.ts
@@ -3,8 +3,9 @@ import en from "./en.json";
 import fr from "./fr.json";
 import tr from "./tr.json";
 import ptBr from "./pt-BR.json";
+import ru from "./ru.json";
 
-export type TranslationKey = "en" | "fr" | "pt-BR" | "tr";
+export type TranslationKey = "en" | "fr" | "pt-BR" | "tr" | "ru";
 export type Translations = {
   [key in TranslationKey]: KeyValueSet<string>
 };
@@ -14,6 +15,7 @@ const trads: Translations = {
   fr,
   "pt-BR": ptBr,
   tr,
+  ru,  
 };
 
 export default trads;
