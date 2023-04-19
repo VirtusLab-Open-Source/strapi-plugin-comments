4a41bb73a778569ed293b5bd2427f17c2d950d61
huangwenbo <huangwenbo@yiyetech.com>
2023-04-19 17:38:05 +0800

    add: Chinese translation

--- admin/src/translations/index.ts
+++ admin/src/translations/index.ts
@@ -3,8 +3,9 @@ import en from "./en.json";
 import fr from "./fr.json";
 import tr from "./tr.json";
 import ptBr from "./pt-BR.json";
+import zhHans from "./zh-Hans.json";
 
-export type TranslationKey = "en" | "fr" | "pt-BR" | "tr";
+export type TranslationKey = "en" | "fr" | "pt-BR" | "tr" | "zh-Hans";
 export type Translations = {
   [key in TranslationKey]: KeyValueSet<string>
 };
@@ -14,6 +15,7 @@ const trads: Translations = {
   fr,
   "pt-BR": ptBr,
   tr,
+  "zh-Hans": zhHans,
 };
 
 export default trads;
