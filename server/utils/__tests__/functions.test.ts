8cdb699eb5d1ec86a5b52619c8ec692892682919
CodeVoyager <tmsz.dnl.puch@gmail.com>
2024-03-02 09:56:34 +0100

    chore: tests of lifecycle hooks coverage

--- server/utils/__tests__/functions.test.ts
+++ server/utils/__tests__/functions.test.ts
@@ -1,10 +1,15 @@
+import { StrapiContext } from "strapi-typed";
 import { setupStrapi, resetStrapi } from "../../../__mocks__/initSetup";
 import {
   assertNotEmpty,
   assertParamsPresent,
+  buildAllHookListeners,
+  buildHookListener,
   getPluginService,
   parseParams,
 } from "../functions";
+import { ContentType, LifeCycleEvent, LifeCycleHookName } from "../types";
+import { IServiceCommon, ToBeFixed } from "../../../types";
 
 beforeEach(setupStrapi);
 afterEach(resetStrapi);
@@ -48,6 +53,79 @@ describe("Test plugin functions utils", () => {
       expect(() => assertNotEmpty(0)).toThrow();
       expect(() => assertNotEmpty(1)).not.toThrow();
       expect(() => assertNotEmpty({ id: 1 })).not.toThrow();
+    });
+  });
+  describe("buildAllHookListeners()", () => {
+    it("should define a listener for each available model lifecycle hook", () => {
+      // Then
+      expect(buildAllHookListeners("comment", {} as StrapiContext))
+        .toMatchInlineSnapshot(`
+        {
+          "afterCount": [Function],
+          "afterCreate": [Function],
+          "afterCreateMany": [Function],
+          "afterDelete": [Function],
+          "afterDeleteMany": [Function],
+          "afterFindMany": [Function],
+          "afterFindOne": [Function],
+          "afterUpdate": [Function],
+          "afterUpdateMany": [Function],
+          "beforeCount": [Function],
+          "beforeCreate": [Function],
+          "beforeCreateMany": [Function],
+          "beforeDelete": [Function],
+          "beforeDeleteMany": [Function],
+          "beforeFindMany": [Function],
+          "beforeFindOne": [Function],
+          "beforeUpdate": [Function],
+          "beforeUpdateMany": [Function],
+        }
+      `);
+    });
+  });
+
+  describe("buildHookListener()", () => {
+    it("should delegate lifecycle hook event to defined listeners", async () => {
+      // Given
+      const contentTypeName: ContentType = "comment";
+      const service: Partial<IServiceCommon> = {
+        runLifecycleHook: jest.fn(),
+      };
+      const plugin: ToBeFixed = {
+        service() {
+          return service;
+        },
+      };
+      const context: StrapiContext = {
+        strapi: {
+          plugin() {
+            return plugin;
+          },
+        } as unknown as StrapiContext["strapi"],
+      };
+      const hookName: LifeCycleHookName = "afterCreate";
+      const event: Partial<LifeCycleEvent> = {
+        action: hookName,
+        model: {
+          attributes: {
+            name: "name",
+          },
+        } as unknown as LifeCycleEvent["model"],
+      };
+      const [, listener] = buildHookListener(
+        contentTypeName,
+        context
+      )(hookName);
+
+      // When
+      await listener(event as LifeCycleEvent);
+
+      // Then
+      expect(service.runLifecycleHook).toHaveBeenCalledWith({
+        contentTypeName,
+        hookName,
+        event,
+      });
     });
   });
 });
