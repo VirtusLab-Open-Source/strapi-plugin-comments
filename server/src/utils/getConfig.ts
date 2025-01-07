d865f9687fabebc89871739d42587ceca1cfe192
Mateusz Ziarko <mziarko@virtuslab.com>
2025-01-07 08:33:22 +0100

    fix: get configs types mismatch

--- server/src/utils/getConfig.ts
+++ server/src/utils/getConfig.ts
@@ -1,11 +1,11 @@
-import { StrapiContext } from '../@types';
+import { StrapiContext, PathValue } from '../@types';
 import { CommentsPluginConfig } from '../config';
 import { getStoreRepository } from '../repositories';
 
 export const getConfig = async <T extends keyof CommentsPluginConfig>(
   strapi: StrapiContext['strapi'],
   path: T,
-  defaultValue: CommentsPluginConfig[T],
+  defaultValue: PathValue<CommentsPluginConfig, T>,
 ): Promise<CommentsPluginConfig[T]> => {
   return getStoreRepository(strapi).getLocalConfig(path, defaultValue);
 };
