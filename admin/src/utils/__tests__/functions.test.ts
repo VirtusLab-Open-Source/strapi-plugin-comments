8127d605b32a41d27155d9845f1c40cacc156fc8
Trochonovitz <k4vq7eku9@pm.me>
2026-07-27 13:17:48 +0200

    fix: add tests for function util assertNotEmpty

--- admin/src/utils/__tests__/functions.test.ts
+++ admin/src/utils/__tests__/functions.test.ts
@@ -1,4 +1,4 @@
-import { assertString } from "../functions";
+import { assertNonEmpty, assertString } from "../functions";
 
 describe("assertString()", () => {
   it("should allow strings", () => {
@@ -11,5 +11,16 @@ describe("assertString()", () => {
     expect(() => assertString(undefined)).toThrow();
     expect(() => assertString({})).toThrow();
     expect(() => assertString(new Error("Error"))).toThrow();
+  });
+});
+
+describe("assertNonEmpty()", () => {
+  it("should allow non-empty values", () => {
+    expect(() => assertNonEmpty("Content")).not.toThrow();
+  });
+
+  it("should reject empty values", () => {
+    expect(() => assertNonEmpty(null)).toThrow();
+    expect(() => assertNonEmpty(undefined)).toThrow();
   });
 });
